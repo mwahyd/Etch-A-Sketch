bb9831a11f00de1ccdc1fe2e867aee3c448692bb
mwahyd <72066821+mwahyd@users.noreply.github.com>
2022-11-09 16:04:57 +0100

    add colour into grid with mouse down event, remove hover effect once a box is coloured

--- scripts.js
+++ scripts.js
@@ -6,10 +6,21 @@ function createGrid() {
   // create a 16x16 grid
   for (const x of Array(256).keys()) {
     const div = document.createElement("div");
-    div.classList.add("block-style");
-    console.log(div);
+    div.classList.add("block-style", "box", "hover-effect");
     gridContainer.appendChild(div);
   }
 }
 
+function colourBox(event) {
+  const box = event.target;
+  box.classList.add("colour-in");
+  box.classList.remove("hover-effect");
+}
+
+// TODO: colour in a box when clicked
 createGrid();
+// event listeners
+const gridBox = document.querySelectorAll(".box");
+gridBox.forEach((box) => {
+  box.addEventListener("mousedown", colourBox);
+});
