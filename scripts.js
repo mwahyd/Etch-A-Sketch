7329619abb88ebabd5281525fa4e7f276715b115
mwahyd <72066821+mwahyd@users.noreply.github.com>
2022-11-09 17:11:40 +0100

    add the clear canvas functionality when clear canvas button is clicked

--- scripts.js
+++ scripts.js
@@ -13,14 +13,30 @@ function createGrid() {
 
 function colourBox(event) {
   const box = event.target;
+  console.log(box);
   box.classList.add("colour-in");
   box.classList.remove("hover-effect");
 }
 
-// TODO: colour in a box when clicked
+function clearCanvas(list) {
+  list.forEach((item) => {
+    if (item.classList.contains("colour-in")) {
+      item.classList.remove("colour-in");
+    }
+  });
+}
+
 createGrid();
+
 // event listeners
 const gridBox = document.querySelectorAll(".box");
+const clearButton = document.querySelector("#clear-btn");
+
 gridBox.forEach((box) => {
   box.addEventListener("mousedown", colourBox);
+  //   box.addEventListener("mouseover", colourBox);
+});
+
+clearButton.addEventListener("click", (e) => {
+  clearCanvas(gridBox);
 });
